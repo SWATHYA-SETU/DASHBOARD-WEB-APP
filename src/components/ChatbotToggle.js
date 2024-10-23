b3c0a23134c49116ea06accb87a550c7bfa7f638
KnowledgeSeekerOnGitHub <KnowledgeSeekerOnG@gmail.com>
2024-10-23 10:02:15 +0530

    Merged with ACEAPP

--- src/components/ChatbotToggle.js
+++ src/components/ChatbotToggle.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Chatbot from './Chatbot';
 import './ChatbotToggle.css'; // Add a new CSS file for toggle animation
+import robotIcon from '../images/robot.png'; // Import the robot image
 
 const ChatbotToggle = () => {
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
@@ -11,10 +12,10 @@ const ChatbotToggle = () => {
 
   return (
     <div className="chatbot-toggle">
-      <button onClick={toggleChatbot} className="toggle-button">
-        💬 Chat with us
-      </button>
-      <div className={`chatbot-wrapper ${isChatbotOpen ? 'open' : 'closed'}`}>
+        <button onClick={toggleChatbot} className="toggle-button">
+            <img src={robotIcon} alt="Chatbot" className="robot-logo"/>
+        </button>
+        <div className={`chatbot-wrapper ${isChatbotOpen ? 'open' : 'closed'}`}>
         {isChatbotOpen && <Chatbot onClose={toggleChatbot} />}
       </div>
     </div>
